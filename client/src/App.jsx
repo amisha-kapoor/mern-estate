cc18f27f6d0f6fcbf7674a89af4b402e3fe90bc9
amisha-kapoor <amisha1203@gmail.com>
2024-01-29 15:28:27 +0530

    complete profile page ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,12 @@
-
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { SignIn } from './pages/SignIn'
-import { SignUp } from './pages/SignUp'
-import { About } from './pages/About'
-import { Profile } from './pages/Profile'
-import { Home } from './pages/Home'
-import { Header } from './components/Header'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import SignUp from './pages/SignUp';
+import SignIn from './pages/SignIn';
+import About from './pages/About';
+import Profile from './pages/Profile';
+import Header from './components/Header';
 import PrivateRoute from './components/PrivateRoute';
+
 export default function App() {
   return (
     <BrowserRouter>
